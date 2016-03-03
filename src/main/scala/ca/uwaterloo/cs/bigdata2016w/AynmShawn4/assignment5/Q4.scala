2233df063c1c07c12b55469efb5f6f05cb166c97
Xiong <x33tang@uwaterloo.ca>
2016-03-02 23:29:37 -0500

    1-5

--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/AynmShawn4/assignment5/Q4.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/AynmShawn4/assignment5/Q4.scala
@@ -6,7 +6,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.rogach.scallop._
 
-object Q3  {
+object Q4  {
   val log = Logger.getLogger(getClass().getName())
 
   def main(argv: Array[String]) {
@@ -19,36 +19,35 @@ object Q3  {
     val sc = new SparkContext(conf)
     val shipdate = args.date().toString.split('-')
 
+
     val lineitem = sc.textFile(args.input()+"/lineitem.tbl")
 
-    //var hashmapPart = HashMap[Int,String]()
-    val part = sc.textFile(args.input()+"/part.tbl")
+    val nation = sc.textFile(args.input()+"/nation.tbl")
         .map(line => {
             val tokens = line.split('|')
-          //  hashmapPart += (tokens(0).toInt -> tokens(1).toString)
             (tokens(0).toInt, tokens(1).toString)
             }).collectAsMap
-    val broadcastPart = sc.broadcast(part)
+    val broadcastNation = sc.broadcast(nation)
 
 
-
-    val supplier = sc.textFile(args.input()+"/supplier.tbl")
+    val customer = sc.textFile(args.input()+"/customer.tbl")
         .map(line => {
             val tokens = line.split('|')
-            (tokens(0).toInt , tokens(1).toString)
+            (tokens(0).toInt , tokens(3).toInt)
 
             }).collectAsMap
-    val broadcastSupplier = sc.broadcast(supplier)
+    val broadcastCustomer = sc.broadcast(customer)
 
+    val orders = sc.textFile(args.input()+"/orders.tbl")
 
     val outcome = lineitem
     	.map( line => {
             val tokens = line.split('|')
-            (tokens(0).toInt, tokens(1).toInt, tokens(2).toInt, tokens(10)) 
+            (tokens(0).toInt, tokens(10)) 
             } )
     	.filter( 
     		l => {
-                 val line = l._4.toString.split('-')
+                 val line = l._2.toString.split('-')
 
     			if (shipdate.length == 3){
     				(shipdate(0) == line(0)) && (shipdate(1) == line(1)) && (shipdate(2) == line(2))
@@ -61,25 +60,36 @@ object Q3  {
     				(shipdate(0) == line(0))
     			}
     			})
-        .map(
-            p => {
-                if ((broadcastSupplier.value.contains(p._3)) && ((broadcastPart.value.contains(p._2)))) {
-                    (p._1, (broadcastSupplier.value(p._3) , broadcastPart.value(p._2) ) )
-                } else {
-                    (-1, (0,0))
-                }
-                })
+        .cogroup(
+            orders.map( line => (line.split('|')(0).toInt, line.split('|')(1).toInt ))
+             .map(
+                p => {
+                    val cp = broadcastCustomer.value
+                    (p._1, cp(p._2))
+                    }
+                )
+            )
         .filter(
             p => {
-                if (p._1 == -1){
+                if (p._2._1.toList.length == 0){
                     false
                 } else {
                     true
                 }
                 })
+        .flatMap(p => {
+            var temp = p._2._1.toList
+            for (i <- 0 to (temp.length - 1) ) yield (p._1, (temp(i), p._2._2))
+        })
+        .map(
+            p => {
+                val np = broadcastNation.value
+                ((p._2._2.toList(0), np(p._2._2.toList(0))), 1)
+                })
+        .reduceByKey(_ + _)
         .sortByKey()
-        .take(20)
-        .map(p => println("("+p._1 + "," +p._2._2 +"," + p._2._1 +")"))
+        .collect()
+        .foreach(p => println("("+p._1._1 + "," + p._1._2 + ","+ p._2 + ")"))
 	}
 
 }
--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/AynmShawn4/assignment5/Q5.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/AynmShawn4/assignment5/Q5.scala
@@ -1,1 +1,104 @@
-package ca.uwaterloo.cs.bigdata2016w.AynmShawn4.assignment5;
+package ca.uwaterloo.cs.bigdata2016w.AynmShawn4.assignment5
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+
+class Config1(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input)
+  val input = opt[String](descr = "input path", required = true)
+}
+
+object Q5  {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new Config1(argv)
+
+    log.info("Input: " + args.input())
+
+    val conf = new SparkConf().setAppName("A5Q5")
+    val sc = new SparkContext(conf)
+
+
+    val lineitem = sc.textFile(args.input()+"/lineitem.tbl")
+
+    val nation = sc.textFile(args.input()+"/nation.tbl")
+        .map(line => {
+            val tokens = line.split('|')
+            (tokens(0).toInt, tokens(1).toString)
+            }).collectAsMap
+    val broadcastNation = sc.broadcast(nation)
+
+
+    val customer = sc.textFile(args.input()+"/customer.tbl")
+        .map(line => {
+            val tokens = line.split('|')
+            (tokens(0).toInt , tokens(3).toInt)
+
+            }).collectAsMap
+    val broadcastCustomer = sc.broadcast(customer)
+
+    val orders = sc.textFile(args.input()+"/orders.tbl")
+
+    val outcome = lineitem
+    	.map( line => {
+            val tokens = line.split('|')
+            (tokens(0).toInt, tokens(10).substring(0,7)) 
+            } )
+        .cogroup(
+            orders.map( line => (line.split('|')(0).toInt, line.split('|')(1).toInt ))
+             .map(
+                p => {
+                    val cp = broadcastCustomer.value
+                    (p._1, cp(p._2))
+                    }
+                )
+             .filter(
+                p => (
+                    if ((p._2 == 3) || (p._2 == 24)) {
+                        true
+                    } else {
+                        false
+                    }
+                    ) 
+                )
+            )
+        .filter(
+            p =>
+            ( if (p._2._2.toList.length == 0){
+                false
+            } else {
+                true
+            }
+                ))
+        .flatMap(p => {
+            var temp = p._2._1.toList
+            for (i <- 0 to (temp.length - 1) ) yield (( temp(i), p._2._2.toList(0)), 1)
+        })
+        .reduceByKey(_ + _)
+        .sortBy(p => (p._1))
+        .collect()
+
+        println("CANADA")
+        outcome
+        .foreach(p =>  {
+            if (p._1._2.toInt == 3){
+                println("("+p._1._1 + "," + p._2 + ")")
+            } 
+            })
+        println("USA")
+        outcome
+        .foreach(p =>  {
+         
+            if (p._1._2.toInt == 24){
+                println("("+p._1._1 + "," + p._2 + ")")
+
+            }
+            })
+
+	}
+
+}
