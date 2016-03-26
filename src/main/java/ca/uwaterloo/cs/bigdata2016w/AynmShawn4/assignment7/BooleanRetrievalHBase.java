1046abc582576415e4c0be1b949e8d4b69d43377
AynmShawn4 <x33tang@uwaterloo.ca>
2016-03-26 17:47:13 -0400

    Create BooleanRetrievalHBase.java

---
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/AynmShawn4/assignment7/BooleanRetrievalHBase.java
@@ -0,0 +1,232 @@
+package ca.uwaterloo.cs.bigdata2016w.AynmShawn4.assignment7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.EOFException;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeSet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.Vector;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.MapFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+
+import tl.lin.data.array.ArrayListWritable;
+import tl.lin.data.fd.Object2IntFrequencyDistribution;
+import tl.lin.data.fd.Object2IntFrequencyDistributionEntry;
+import tl.lin.data.pair.PairOfInts;
+import tl.lin.data.pair.PairOfObjectInt;
+import tl.lin.data.pair.PairOfWritables;
+import tl.lin.data.pair.PairOfStringInt;
+import org.apache.hadoop.io.VIntWritable;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.WritableUtils;
+
+public class BooleanRetrievalCompressed extends Configured implements Tool {
+  private Vector <MapFile.Reader> index = new Vector <MapFile.Reader>();
+  private FSDataInputStream collection;
+  private Stack<Set<Integer>> stack;
+  private int red  = 0;
+
+  private BooleanRetrievalCompressed() {}
+
+  private void initialize(String indexPath, String collectionPath, FileSystem fs) throws IOException {
+
+
+    Path path = new Path( indexPath );
+    ContentSummary cs = fs.getContentSummary(path);
+    red = (int)cs.getDirectoryCount() - 1;
+
+    for (int i =0; i < red; i++){
+      MapFile.Reader aaa = new MapFile.Reader(new Path(indexPath + "/part-r-0000" + i), fs.getConf());
+      index.add(aaa);
+    }
+    collection = fs.open(new Path(collectionPath));
+    stack = new Stack<Set<Integer>>();
+  }
+
+  private void runQuery(String q) throws IOException {
+    String[] terms = q.split("\\s+");
+
+    for (String t : terms) {
+      if (t.equals("AND")) {
+        performAND();
+      } else if (t.equals("OR")) {
+        performOR();
+      } else {
+        pushTerm(t);
+      }
+    }
+
+    Set<Integer> set = stack.pop();
+
+    for (Integer i : set) {
+      String line = fetchLine(i);
+      System.out.println(i + "\t" + line);
+    }
+  }
+
+  private void pushTerm(String term) throws IOException {
+    stack.push(fetchDocumentSet(term));
+  }
+
+  private void performAND() {
+    Set<Integer> s1 = stack.pop();
+    Set<Integer> s2 = stack.pop();
+
+    Set<Integer> sn = new TreeSet<Integer>();
+
+    for (int n : s1) {
+      if (s2.contains(n)) {
+        sn.add(n);
+      }
+    }
+
+    stack.push(sn);
+  }
+
+  private void performOR() {
+    Set<Integer> s1 = stack.pop();
+    Set<Integer> s2 = stack.pop();
+
+    Set<Integer> sn = new TreeSet<Integer>();
+
+    for (int n : s1) {
+      sn.add(n);
+    }
+
+    for (int n : s2) {
+      sn.add(n);
+    }
+
+    stack.push(sn);
+  }
+
+  private Set<Integer> fetchDocumentSet(String term) throws IOException {
+    Set<Integer> set = new TreeSet<Integer>();
+
+    for (PairOfInts pair : fetchPostings(term)) {
+      set.add(pair.getLeftElement());
+    }
+
+    return set;
+  }
+
+  private ArrayListWritable<PairOfInts> fetchPostings(String term) throws IOException {
+    Text key = new Text();
+
+    key.set(term);
+    int previous = 0;
+
+    BytesWritable bytesData = new BytesWritable();
+
+    int partition = (term.toString().hashCode() & Integer.MAX_VALUE) % red;
+    index.elementAt(partition).get(key, bytesData);
+
+    ArrayListWritable<PairOfInts> posting = new ArrayListWritable<PairOfInts>();
+
+    byte[] bytes = bytesData.getBytes();
+    DataInputStream data = new DataInputStream(new ByteArrayInputStream(bytes));
+
+    int docno = 0;
+    int tf = 0;
+    int eof = 1; 
+    while ( eof == 1 ){
+      docno = WritableUtils.readVInt(data) ;
+      docno += previous;
+      tf = WritableUtils.readVInt(data);
+
+      if ((docno == previous) || (tf == 0)){
+        eof = 0;
+      } else {
+        previous = docno;
+
+        posting.add(new PairOfInts(docno, tf) );
+
+      }
+
+    }
+
+    return posting;
+  }
+
+  public String fetchLine(long offset) throws IOException {
+    collection.seek(offset);
+    BufferedReader reader = new BufferedReader(new InputStreamReader(collection));
+
+    String d = reader.readLine();
+
+    return d.length() > 80 ? d.substring(0, 80) + "..." : d;
+  }
+
+  public static class Args {
+    @Option(name = "-index", metaVar = "[path]", required = true, usage = "index path")
+    public String index;
+
+    @Option(name = "-collection", metaVar = "[path]", required = true, usage = "collection path")
+    public String collection;
+
+    @Option(name = "-query", metaVar = "[term]", required = true, usage = "query")
+    public String query;
+  }
+
+  /**
+   * Runs this tool.
+   */
+  public int run(String[] argv) throws Exception {
+    Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
+    try {
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      return -1;
+    }
+
+    if (args.collection.endsWith(".gz")) {
+      System.out.println("gzipped collection is not seekable: use compressed version!");
+      return -1;
+    }
+
+    FileSystem fs = FileSystem.get(new Configuration());
+
+    initialize(args.index, args.collection, fs);
+
+    System.out.println("Query: " + args.query);
+    long startTime = System.currentTimeMillis();
+    runQuery(args.query);
+    System.out.println("\nquery completed in " + (System.currentTimeMillis() - startTime) + "ms");
+
+    return 1;
+  }
+
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new BooleanRetrievalCompressed(), args);
+  }
+}
