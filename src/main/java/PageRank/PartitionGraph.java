077a580775decb233ddb6c3a8b8d8913f4cea376
AynmShawn4 <x33tang@uwaterloo.ca>
2016-09-03 23:14:35 -0400

    Adding code

--- src/main/java/PageRank/PartitionGraph.java
+++ src/main/java/PageRank/PartitionGraph.java
@@ -118,9 +118,9 @@ public class PartitionGraph extends Configured implements Tool {
     job.setOutputKeyClass(IntWritable.class);
     job.setOutputValueClass(PageRankNode.class);
 
-   // if (useRange) {
+
       job.setPartitionerClass(RangePartitioner.class);
-   // }
+ 
 
     FileSystem.get(conf).delete(new Path(outPath), true);
 
